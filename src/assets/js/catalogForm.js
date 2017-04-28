2431cf439dc8f287c0913c034f673a614fbcb1da
Kevin N. O'Riley <knoriley@basspro.com>
2017-04-28 16:42:19 -0500

    half the form styles are done, started getting monotonous, so moving on for now.

--- src/assets/js/catalogForm.js
+++ src/assets/js/catalogForm.js
@@ -1,18 +1,40 @@
-jQuery('input').each(function(){
-  var sib = jQuery(this).siblings("span");
-  jQuery(sib).css({
-    'top':"36px"
-  })
-});
-
-jQuery('input').on('touhstart click', function(){
-  var sib = jQuery(this).siblings("span");
-  jQuery(sib).animate({
-      top:'15px'
-  },300);
+jQuery('label').each(function(){
+  var spanText = jQuery(this).html();
+  var parent = jQuery(this).parent();
+  var inputSibling = jQuery(this).siblings("input").attr("type");
+  if(inputSibling != 'checkbox'){
+  jQuery(this).hide();
+  jQuery(parent).append("<span>"+spanText+"</span>");
+}
 });
 
 
+// jQuery('input').each(function(){
+//   var sib = jQuery(this).siblings("span");
+//   jQuery(sib).css({
+//     'top':"36px",
+//     
+//     
+//   })
+// });
+
+jQuery('input, span').on('touchstart click', function(){
+  var whatElement = jQuery(this);
+  // console.log(whatElement.context.localName);
+  if(whatElement.context.localName === 'span'){
+    jQuery(this).siblings('input').focus();
+    jQuery(this).animate({
+        top:'15px'
+    },300);
+  }
+  if(whatElement.context.localName ==='input'){
+    var sib = jQuery(this).siblings("span");
+    jQuery(sib).animate({
+        top:'15px'
+    },300);
+  }
+  
+});
 
 jQuery('input').on("blur", function(){
   var val = jQuery(this).val()
