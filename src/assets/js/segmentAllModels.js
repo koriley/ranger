55658284c3e294893316a37a010477b27ae00db3
Kevin N. O'Riley <knoriley@basspro.com>
2017-05-02 08:10:46 -0500

    Fixed the menu scrolling where magellan wouldent update which area you were scrolling in.

--- src/assets/js/segmentAllModels.js
+++ src/assets/js/segmentAllModels.js
@@ -1,6 +1,51 @@
-jQuery('.stickyMenu a').click(function(){
-  jQuery('.fakeActive').each(function(){
-      jQuery(this).removeClass('fakeActive');
-  });
-  jQuery(this).addClass('fakeActive');
+jQuery('.stickyMenu li').click(function() {
+    jQuery('.scrollActive').each(function() {
+        jQuery(this).removeClass('scrollActive');
+    });
+    jQuery(this).addClass('scrollActive');
+});
+
+var lastId,
+    topMenu = jQuery("#magellanMenu"),
+    topDistance = jQuery(".segmentHero"), 
+    topMenuHeight = topDistance.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function() {
+        var item = jQuery(jQuery(this).attr("href"));
+        if (item.length) {
+            return item;
+        }
+    });
+
+// Bind to scroll
+jQuery(window).scroll(function() {
+    // Get container scroll position
+    var fromTop = jQuery(this).scrollTop();
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function() {
+        if (jQuery(this).offset().top < fromTop) 
+            return this;
+        }
+    );
+    
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+  
+    var id = cur && cur.length
+        ? cur[0].id
+        : "";
+    if(id === ''){
+      topMenu.children("li").first().addClass("scrollActive");
+    }
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        
+        menuItems.parent().removeClass("scrollActive").end().filter("[href='#" + id + "']").parent().addClass("scrollActive");
+    }
+  
+    
 });
